e718f00de176303ad5133ef97aa77288d0b90f50
狩野秀雄 <karino@karino-no-MacBook-Air.local>
2018-05-31 15:46:15 +0900

    add new file

---
+++ page-requirements.php
@@ -0,0 +1,291 @@
+<?php
+/*
+Template Name: recruit_requirements
+*/
+?>
+
+<?php get_header('recruit'); ?>
+   
+    <div id="requirement" class="container">
+        <div class="contents">
+            <div id="requirement-top">
+                <h1>募集要項</h1>
+                <h2>男女も年齢も関係なく、誰もが活躍できるアットホームな職場</h2>
+            </div>
+            <div id="requirement-table">
+                <h1>東京・中野区立保育園オープニング・スタッフ募集</h1>
+                <h2>２０１８年４月開園。保育英検有資格の保育士による英語を使った保育を一部行います。</h2>
+                <table>
+                    <tr>
+                        <td class="requirement-th">募集職種</td>
+                        <td colspan="2">
+<!--                            <p>保育スタッフ（保育士・看護師）</p> -->
+                            <p>保育スタッフ（保育士）</p>
+<!--                            <p>給食室スタッフ（栄養士）</p> -->
+<!--                            <p>給食室スタッフ</p>       -->                     
+<!--                           <p>施設長（園長）</p> -->
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="requirement-th">勤務地</td>
+                        <td colspan="2">
+                            <p><a href="https://www.google.com/maps/place/%E3%80%92165-0026+%E6%9D%B1%E4%BA%AC%E9%83%BD%E4%B8%AD%E9%87%8E%E5%8C%BA%E6%96%B0%E4%BA%95%EF%BC%92%E4%B8%81%E7%9B%AE%EF%BC%93%EF%BC%91/@35.709377,139.663759,17z/data=!4m5!3m4!1s0x6018f29afa124f81:0x7b2215672dfe8b0e!8m2!3d35.7105704!4d139.6625798?hl=ja">中野区立 新井２丁目保育室</a></p>
+                            <p><a href="https://www.google.com/maps?ll=35.718945,139.664813&z=16&t=m&hl=ja&gl=JP&mapclient=embed&q=%E3%80%92165-0025+%E6%9D%B1%E4%BA%AC%E9%83%BD%E4%B8%AD%E9%87%8E%E5%8C%BA%E6%B2%BC%E8%A2%8B%EF%BC%91%E4%B8%81%E7%9B%AE%EF%BC%98%E2%88%92%EF%BC%91%EF%BC%95">中野区立 沼袋１丁目保育室</a></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="requirement-th">勤務時間</td>
+                        <td colspan="2">
+                            <p>7:00~19:30 シフト制（開所時間のうち1日8時間＋1時間休憩）</p>
+                            <p>平日・土曜（月1、2回）出勤、日曜・祝祭日休み☆シフトに子育て・介護の時間帯を配慮</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="requirement-th" rowspan="6">給与（正社員）</td>
+                        <td colspan="2">
+                            <p>＜モデル月給＞</p>
+                            <p>新卒・保育士・専門学校卒業見込の場合：月給200,000円</p>
+                            <p>中途・保育士・保育経験7年・副主任の場合：月給235,000円</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="requirement-salary">
+                            <p>【基本給】</p>
+                        </td>
+                        <td>
+                            <p>新卒/既卒：（専門学校・短大）170,000円 （四年制大学）180,000円</p>
+                            <p>中途：180,000～210,000円（経験・能力による）</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="requirement-salary">
+                            <p>【資格手当】</p>
+                        </td>
+                        <td>
+                            <p>保育士：20,000円、看護師/保健師：30,000円</p>
+                            <p>管理栄養士：10,000円、栄養士：5,000円</p>
+                            <p>保育英検：（3級）2,000円 （2級）5,000円 （準1級以上）10,000円</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="requirement-salary">
+                            <p>【役職手当】</p>
+                        </td>
+                        <td>
+                            <p>園長：60,000円、主任：40,000円、副主任：20,000円</p>
+                            <p>※主任は月20時間分、副主任は月10時間分の残業代含む。</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="requirement-salary">
+                            <p>【地域手当】</p>
+                        </td>
+                        <td>
+                            <p>東京23区内勤務：10,000円</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="requirement-salary">
+                            <p>【通勤手当】</p>
+                        </td>
+                        <td>
+                            <p>月20,000円上限にて支給</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="requirement-th">
+                            <p>給与（パート）</p>
+                        </td>
+                        <td colspan="2">
+<!--                            <p>保育士：時給1,100円～ 看護師：時給1,500円～</p>
+                            <p>栄養士：時給1,050円～ その他：時給1,000円～</p>
+                            <p>保育士：時給1,100円～ 看護師：時給1,500円～</p> -->
+                            <p>保育士：時給1,050円～ その他：時給1,000円～</p>                            
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="requirement-th">
+                            <p>昇給</p>
+                        </td>
+                        <td colspan="2">
+                            <p>年1回（4月）</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="requirement-th">
+                            <p>賞与</p>
+                        </td>
+                        <td colspan="2">
+                            <p>年2回（2016年度実績：2.0ヶ月） ☆ほか業績により年度末臨時賞与あり</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="requirement-th">
+                            <p>休日/休暇</p>
+                        </td>
+                        <td colspan="2">
+                            <p>年間120日（有休除く）</p>
+                            <p>特別休暇（4日）、年末年始休暇（12/29～1/3）育児休業制度・介護休業制度あり</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="requirement-th">
+                            <p>社宅</p>
+                        </td>
+                        <td colspan="2">
+                            <p>借上げ社宅制度あり（家賃補助上限82,000円・最長5年間・常勤保育士対象）</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="requirement-th">
+                            <p>福利厚生</p>
+                        </td>
+                        <td colspan="2">
+                            <p>・各種社会保険完備 ・定期健康診断 ・慶弔金支給</p>
+                            <p>・ユニフォーム貸与 ・海外研修制度 ・クラブ活動（一部補助あり）</p>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <div id="requirement-table">
+                <h1>横浜市保育園・スタッフ募集</h1>
+                <h2>横浜市（都筑区/緑区）の保育園。センター北の2園は２０１８年４月リニューアル・オープン。</h2>
+                <table>
+                    <tr>
+                        <td class="requirement-th">募集職種</td>
+                        <td colspan="2">
+                            <p>保育スタッフ（保育士）</p>
+                            <p>栄養士（センター北園）
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="requirement-th">勤務地</td>
+                        <td colspan="2">
+                            <p><a href="#">認可保育園キッズフォレセンター北</a></p>
+                            <p><a href="#">認可保育園キッズフォレ長津田</a></p>
+                            <p><a href="#">認可保育園キッズフォレ東山田</a></p>                            
+                            <p><a href="https://www.google.com/maps?ll=35.550483,139.578907&z=16&t=m&hl=ja&gl=JP&mapclient=embed&q=%E3%80%92224-0003+%E7%A5%9E%E5%A5%88%E5%B7%9D%E7%9C%8C%E6%A8%AA%E6%B5%9C%E5%B8%82%E9%83%BD%E7%AD%91%E5%8C%BA%E4%B8%AD%E5%B7%9D%E4%B8%AD%E5%A4%AE%EF%BC%91%E4%B8%81%E7%9B%AE%EF%BC%91%EF%BC%99%E2%88%92%EF%BC%91%EF%BC%99">横浜保育室キッズフォレアカデミー</a></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="requirement-th">勤務時間</td>
+                        <td colspan="2">
+                            <p>7:00~20:00 シフト制（開所時間のうち1日8時間＋1時間休憩）</p>
+                            <p>平日・土曜（月1、2回）出勤、日曜・祝祭日休み☆シフトに子育て・介護の時間帯を配慮</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="requirement-th" rowspan="5">給与（正社員）</td>
+                        <td class="border-dash" colspan="2">
+                            <p>＜モデル月給＞</p>
+                            <p>新卒・保育士・専門学校卒業見込の場合：月給190,000円</p>
+                            <p>中途・保育士・保育経験7年・副主任の場合：月給225,000円</p>
+                        </td>
+                    </tr>
+                    <tr class="border-dash">
+                        <td class="requirement-salary">
+                            <p>【基本給】</p>
+                        </td>
+                        <td>
+                            <p>新卒/既卒：（専門学校・短大）170,000円 （四年制大学）180,000円</p>
+                            <p>中途：180,000～210,000円（経験・能力による）</p>
+                        </td>
+                    </tr>
+                    <tr class="border-dash">
+                        <td class="requirement-salary">
+                            <p>【資格手当】</p>
+                        </td>
+                        <td>
+                            <p>保育士：20,000円、看護師/保健師：30,000円</p>
+                            <p>管理栄養士：10,000円、栄養士：5,000円</p>
+                            <p>保育英検：（3級）2,000円 （2級）5,000円 （準1級以上）10,000円</p>
+                        </td>
+                    </tr>
+                    <tr class="border-dash">
+                        <td class="requirement-salary">
+                            <p>【役職手当】</p>
+                        </td>
+                        <td>
+                            <p>主任：40,000円、副主任：20,000円</p>
+                            <p>※主任は月20時間分、副主任は月10時間分の残業代含む。</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="requirement-salary">
+                            <p>【通勤手当】</p>
+                        </td>
+                        <td>
+                            <p>月20,000円上限にて支給</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="requirement-th">
+                            <p>給与（パート）</p>
+                        </td>
+                        <td colspan="2">
+                            <p>保育士　　：時給1,050円～ </p>
+                            <p>バス添乗員：時給1,000円〜</p>
+                            <p>その他（保育補助）：時給960円～</p>
+<!--                            <p>栄養士：時給1,000円～ その他：時給960円～</p>　-->
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="requirement-th">
+                            <p>昇給</p>
+                        </td>
+                        <td colspan="2">
+                            <p>年1回（4月）</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="requirement-th">
+                            <p>賞与</p>
+                        </td>
+                        <td colspan="2">
+                            <p>年2回（2016年度実績：2.0ヶ月） ☆ほか業績により年度末臨時賞与あり</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="requirement-th">
+                            <p>休日/休暇</p>
+                        </td>
+                        <td colspan="2">
+                            <p>年間120日（有休除く）</p>
+                            <p>特別休暇（4日）、年末年始休暇（12/29～1/3）育児休業制度・介護休業制度あり</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="requirement-th">
+                            <p>社宅</p>
+                        </td>
+                        <td colspan="2">
+                            <p>借上げ社宅制度あり（家賃補助上限82,000円・最長10年間・常勤保育士対象）</p>
+                            <p>※制度利用の対象者および補助額については当社規定による</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="requirement-th">
+                            <p>福利厚生</p>
+                        </td>
+                        <td colspan="2">
+                            <p>・各種社会保険完備 ・定期健康診断 ・慶弔金支給</p>
+                            <p>・ユニフォーム貸与 ・海外研修制度 ・クラブ活動（一部補助あり）</p>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+     <div id="bottom-light"> 
+                       <a href="http://www.kidsfore.co.jp/recruit_entry"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/entry_button.png"/></a>
+	</div>
+    <div id="bottom-menu">
+        <ul>
+             <li><a href="<?php echo get_stylesheet_directory_uri(); ?>/images/recruit_movie.mp4"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/kidsfore-btn.png"></a></li>
+            <li><a href="<?php echo get_stylesheet_directory_uri(); ?>/images/athoiku.pdf"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/atorie-btn.png"></a></li>
+            <li><a href="<?php bloginfo('url'); ?>/saiyo"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/blog-btn.png"></a></li>
+        </ul>
+    </div>
+
+
+<?php get_footer('recruit'); ?>
